b135dac026543b3252707cf969927ee7ca4a9029
Maran Sowthri Kalailingam <kmaran@athenahealth.com>
2021-08-28 17:58:24 +0530

    App is ready for deployment

--- server/model/hobby.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const { Schema } = mongoose;
-
-const hobbySchema = new Schema({
-  title: String,
-  userID: String
-});
-
-module.exports = mongoose.model("Hobby", hobbySchema);
--- server/model/post.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const { Schema } = mongoose;
-
-const postSchema = new Schema({
-  content: String,
-  userID: String,
-});
-
-module.exports = mongoose.model("Post", postSchema);
